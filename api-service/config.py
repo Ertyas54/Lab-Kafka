3ed4e15527e27b8db79eb5aea67f430fac0e01a0
Ertyas54 <167956620+Ertyas54@users.noreply.github.com>
2026-05-07 00:22:29 +0400

    remove default values from config, use env variables in docker-compose

--- api-service/config.py
+++ api-service/config.py
@@ -1,5 +1,5 @@
 import os
 
-KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'kafka:29092')
-DATA_SERVICE_URL = os.getenv('DATA_SERVICE_URL', 'http://data-service:8081')
+KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
+DATA_SERVICE_URL = os.getenv('DATA_SERVICE_URL')
 KAFKA_TOPIC = 'medical-appointments'
